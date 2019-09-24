a5cd307251d1f88519bf244738f47475370aad8e
Hyun Kyo Jung <hj283@cornell.edu>
2019-09-24 17:14:55 +0900

    sort ranks and only keep the top 10

--- models/rank.model.js
+++ models/rank.model.js
@@ -4,18 +4,15 @@ const helper = require('../helpers/helper.js')
 
 function getRanks() {
     return new Promise((resolve, reject) => {
-        if (ranks.length === 0) {
-            reject({
-                message: 'no ranks available',
-                status: 202
-            })
-        }
         resolve(ranks)
     })
 }
+
 function insertRank(newRank) {
     return new Promise((resolve, reject) => {
         ranks.push(newRank)
+        ranks.sort((r1, r2) => r2['score'] - r1['score'])
+        ranks = ranks.slice(0, 10);
         helper.writeJSONFile(ranks)
         resolve(newRank)
     })
