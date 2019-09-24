8dbdb3675e717732d133224070a3692dca144a34
Hyun Kyo Jung <hj283@cornell.edu>
2019-09-24 14:15:22 +0900

    cors

--- helpers/middlewares.js
+++ helpers/middlewares.js
@@ -1,11 +1,15 @@
 function checkFieldsRank(req, res, next) {
     console.log("checking if the fields are good");
-    const { score, name } = req.body
-
+    const { score, name } = req.body;
+    console.log("request ");
+    console.log(req)
+    console.log("score" + score);
+    console.log("name" + name);
     if (score && name) {
         console.log("fields are good");
         next()
     } else {
+        console.log("fields are bad")
         res.status(400).json({ message: "fields are not good" })
     }
 
