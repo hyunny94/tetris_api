03a32ffb94d04fd09992037e6810af7f6ab68b95
Hyun Kyo Jung <hj283@cornell.edu>
2019-09-24 13:39:23 +0900

    allowing CORS on OPTIONS requests

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(require('./routes/index.routes'))
 
+//allow OPTIONS on just one resource
+app.options("https://master.d1eay1f6v0z5km.amplifyapp.com", cors())
+
 app.get('/', (req, res) => {
 	res.json({ message: "yo" })
 })
