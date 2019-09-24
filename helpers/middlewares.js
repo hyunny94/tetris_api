22fb115cface02e4b33be7cdafb7e2f2a107113e
Hyun Kyo Jung <hj283@cornell.edu>
2019-09-24 13:21:30 +0900

    console logs added to fix POST CORS issue

--- helpers/middlewares.js
+++ helpers/middlewares.js
@@ -1,7 +1,9 @@
 function checkFieldsRank(req, res, next) {
+    console.log("checking if the fields are good");
     const { score, name } = req.body
 
     if (score && name) {
+        console.log("fields are good");
         next()
     } else {
         res.status(400).json({ message: "fields are not good" })
