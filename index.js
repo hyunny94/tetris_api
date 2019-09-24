5c50c2c5accca28124873a6833b8164a94273dcf
Hyun Kyo Jung <hj283@cornell.edu>
2019-09-24 14:10:51 +0900

    cors

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const app = express()
 app.use(morgan('tiny'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(require('./routes/index.routes'))
 
 app.use(cors())
+
+app.use(require('./routes/index.routes'))
+
+
 
 app.get('/', (req, res) => {
 	res.json({ message: "yo" })
