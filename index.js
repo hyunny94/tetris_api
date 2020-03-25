5b15ddd6fdda3dae4f757c7d43aa58d256bb46a7
Hyun Kyo Jung <hj283@cornell.edu>
2020-03-24 23:08:40 -0400

    added socket functionality. game does not start until an opponent is online. also if a player pauses a game the game gets paused for both players

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const socketIO = require('socket.io')
 
 const app = express()
 app.use(morgan('tiny'))
@@ -19,4 +20,49 @@ app.get('/', (req, res) => {
 	res.json({ message: "yo" })
 })
 
-app.listen('9000')
+const server = app.listen('9000')
+
+const io = socketIO(server)
+
+// players that have been matched with an opponent has an entry in 
+// this dictionary. value is the opponent's socket ID
+const players = {}
+
+// players waiting to be matched with an opponent
+const lockerRoom = []
+
+io.on("connection", socket => {
+	console.log("New client connected");
+	console.log("socket id is", socket.id);
+	// new match
+	if (lockerRoom.length == 1) {
+		var playerOneSocket = lockerRoom.pop();
+		var roomName = playerOneSocket.id + socket.id;
+		console.log("roomName is ", roomName);
+		players[socket.id] = roomName;
+		players[playerOneSocket.id] = roomName;
+		playerOneSocket.join(roomName);
+		socket.join(roomName);
+		io.to(roomName).emit("game start");
+	}
+	// wait in lockerRoom
+	else {
+		lockerRoom.push(socket);
+	}
+
+	socket.on("pause", () => {
+		console.log("a player paused the game");
+		io.to(players[socket.id]).emit("pause");
+	});
+
+	socket.on("unpause", () => {
+		console.log("a player unpaused the game");
+		io.to(players[socket.id]).emit("unpause");
+	});
+	
+	socket.on("disconnect", () => {
+			io.to(players[socket.id]).emit("opponent left");
+			console.log("Client disconnected")
+		}
+	);
+  });
