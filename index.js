a8bb0ce276d04d9180729c35908aee71c18da01c
hyunny94 <hj283@cornell.edu>
2021-01-03 21:22:45 +0900

    Add socket support to enable tetris battle mode.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const uuid = require('uuid')
 
 const app = express()
 app.use(morgan('tiny'))
@@ -8,7 +9,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 const corsOptions = {
-	origin: ['https://master.d1eay1f6v0z5km.amplifyapp.com', 'https://www.kyothrees.com', 'https://kyothrees.com']
+	origin: [
+		'https://master.d1eay1f6v0z5km.amplifyapp.com', 
+		'https://www.kyothrees.com', 
+		'https://kyothrees.com',
+		'http://localhost:3000'
+	]
 }
 
 app.use(cors(corsOptions))
@@ -19,4 +25,63 @@ app.get('/', (req, res) => {
 	res.json({ message: "yo" })
 })
 
-app.listen('9000')
+const socketIO = require('socket.io')(app.listen('9000'), {
+	cors: {
+	  origin: 'http://localhost:3000',
+	  methods: ["GET", "POST"]
+	}
+  })
+
+// Object mapping player socket to active game id
+let playerToGameId = {}
+
+// sockets of players waiting to be matched
+let waitingRoom = []
+
+socketIO.on('connection', (socket) => {
+	const playerId = socket.id
+	console.log("a user connected: ", playerId)
+	 
+	// Quick Match Request
+	socket.on("playTetrisBattleWithSomeone", () => {
+		console.log("Quick match request received. playerId: ", playerId)
+		// if there is someone in the waiting room, match the new user
+		if (waitingRoom.length !== 0) {
+			const opponentSocket = waitingRoom.pop()
+			// create a new unique game id
+			const gameId = uuid.v1()
+			// have the two players join the room 
+			socket.join(gameId)
+			opponentSocket.join(gameId)
+
+			// two players are mapped to the same gameId
+			playerToGameId[socket]         = gameId
+			playerToGameId[opponentSocket] = gameId
+
+			// set a timer for 2 minutes and have it delete the mappings in playerToGameId object 
+			setTimeout(() => {
+				delete playerToGameId.socket
+				delete playerToGameId.opponentSocket
+				socketIO.to(gameId).emit("game over")
+				socket.leave(gameId)
+				opponentSocket.leave(gameId)
+			}, 120000, socket, opponentSocket)
+
+			// Let the players know they are matched
+			socketIO.to(gameId).emit("matched")
+		}
+		// else the new user is put into the waiting room.
+		else {
+			waitingRoom.unshift(socket)
+			console.log(playerId, " entered the waiting room.")
+			socket.emit("entered waiting room")
+		}
+	})
+
+	// User Disconnect
+	socket.on("disconnect", () => {
+		console.log("a user disconnected")
+	})
+})
+
+
--- models/rank.model.js
+++ models/rank.model.js
@@ -4,7 +4,7 @@ const helper = require('../helpers/helper.js')
 
 function getRanks() {
     return new Promise((resolve, reject) => {
-        var top10 = ranks.slice(0, 10);
+        var top10 = ranks.slice(0, 20);
         resolve(top10);
     })
 }
@@ -13,7 +13,7 @@ function insertRank(newRank) {
     return new Promise((resolve, reject) => {
         ranks.push(newRank)
         ranks.sort((r1, r2) => r2['score'] - r1['score'])
-        // ranks = ranks.slice(0, 10);
+        ranks = ranks.slice(0, 20);
         helper.writeJSONFile(ranks)
         resolve(newRank)
     })
