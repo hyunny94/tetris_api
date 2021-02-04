b0d5cae8a65999c5918094f54bd8fe81d39c6e2f
hyunny94 <hj283@cornell.edu>
2021-02-04 21:26:18 +0900

    clean up

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ app.use(morgan('tiny'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+const TETRIS_BATTLE_DURATION_IN_SEC = 120;
+
 const corsOptions = {
 	origin: [
 		'https://master.d1eay1f6v0z5km.amplifyapp.com', 
 		'https://www.kyothrees.com', 
 		'https://kyothrees.com',
 		'http://localhost:3000'
-	]
+	], 
+	methods: ["GET", "POST"]
 }
 
 app.use(cors(corsOptions))
@@ -26,10 +29,7 @@ app.get('/', (req, res) => {
 })
 
 const socketIO = require('socket.io')(app.listen('9000'), {
-	cors: {
-	  origin: 'http://localhost:3000',
-	  methods: ["GET", "POST"]
-	}
+	cors: corsOptions
   })
 
 // Object mapping player socket to active game id
@@ -65,7 +65,7 @@ socketIO.on('connection', (socket) => {
 				// initialize game state
 				gameIdToState[gameId] = {paused: false, pausedBy: null, 
 					pauseTimer: null, pauseSecLeft: null, pauseSecTimer: null, 
-					timeUpdateInterval: null, secLeft: 120, sockets}
+					timeUpdateInterval: null, secLeft: TETRIS_BATTLE_DURATION_IN_SEC, sockets}
 	
 				// Let the players know they are matched
 				socketIO.to(gameId).emit("matched")
